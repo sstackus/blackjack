079de3ea0b101fae2ced33b1759362e2c6597575
Simon Stack <simon@stackus.dev>
2020-08-16 17:43:05 +0100

    Improve win/loss styling

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import Deck from 'components/Deck/Deck';
 import Player from 'components/Player/Player';
-import { Players } from 'containers';
+import { Game, Players } from 'containers';
 
 import './Table.scss';
 
 export default function Table() {
+  const game = Game.useContainer();
+
   return (
-    <div className="Table">
+    <div className={`Table ${game.isWin() && 'Table--win'} ${game.isLoss() && 'Table--loss'}`}>
       <Player className="Table__row" label="Dealer" container={Players[0]} />
       <Deck />
       <Player className="Table__row" container={Players[1]} />
